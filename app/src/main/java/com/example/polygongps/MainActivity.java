e1c2793d1768ebc9dfc3ec8e35ac14ae0569bb50
matheusmnsoares <matheusmontenegro2011@hotmail.com>
2023-11-15 20:34:53 -0300

    Incluindo design (Lediane) e arrumando botões

--- app/src/main/java/com/example/polygongps/MainActivity.java
+++ app/src/main/java/com/example/polygongps/MainActivity.java
@@ -1,18 +1,47 @@
 package com.example.polygongps;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityOptionsCompat;
+import androidx.core.view.ViewCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.transition.Fade;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-    Button showMap;
+        setTitle("Activity Main");
 
+        Fade fade = new Fade();
+        fade.excludeTarget(android.R.id.statusBarBackground, true);
+        fade.excludeTarget(android.R.id.navigationBarBackground, true);
 
+        getWindow().setEnterTransition(fade);
+        getWindow().setExitTransition(fade);
 
+        final ImageView imageView = findViewById(R.id.mainInfo);
+        Button infoButton = findViewById(R.id.buttonInfo);
+        infoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, InfoActivity.class);
+                ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(MainActivity.this, imageView, ViewCompat.getTransitionName(imageView));
+                startActivity(intent, options.toBundle());
+            }
+        });
+    } public void slide(View view) {
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+    }
+}
+    /*
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,4 +58,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
 }
+
+<ImageButton
+        android:id="@+id/mapMenu"
+        android:layout_width="56dp"
+        android:layout_height="53dp"
+        android:layout_marginStart="272dp"
+        android:src="@drawable/menu"
+        app:layout_constraintStart_toStartOf="@+id/mapBack"
+        app:layout_constraintTop_toTopOf="parent" />*/
--- app/src/main/java/com/example/polygongps/MapsActivity.java
+++ app/src/main/java/com/example/polygongps/MapsActivity.java
@@ -6,10 +6,13 @@ import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.FrameLayout;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -17,12 +20,16 @@ import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -31,15 +38,23 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private static final int REQUEST_CODE = 101;
     FrameLayout map;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
         map = findViewById(R.id.map);
+        //verticesEditText = findViewById(R.id.verticesEditText);
+        //captureButton = findViewById(R.id.captureButton);
 
         fusedClient = LocationServices.getFusedLocationProviderClient(this);
         getLocation();
+    }
+
+    public void slide(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 
     private void getLocation(){
@@ -70,7 +85,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
-        MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("You are Here!");
+        MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("Você está aqui!");
         googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
         googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
         googleMap.addMarker(markerOptions);
